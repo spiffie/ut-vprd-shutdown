11603f1d8f0a6bc9b3d7dba14e66a33a03537cd0
Todd Graves <tgraves@austin.utexas.edu>
2014-04-16 15:29:41 -0500

    referencing appropriate view

--- shutdown/urls.py
+++ shutdown/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from shutdown.views import Shutdown
 
 urlpatterns = patterns('',
-    url(r'^shutdown/$', Outage.as_view(), name='shutdown',),
+    url(r'^shutdown/$', Shutdown.as_view(), name='shutdown',),
 )
 
