4aedca1d306d8337aaf88f966a390a11ca612b38
Eric Petersen <eric@huskers.unl.edu>
2015-01-29 14:27:29 -0600

    (FIN-3423) Adding migrations files

---
+++ shutdown/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ShutDown',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('message', models.CharField(max_length=1000)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
